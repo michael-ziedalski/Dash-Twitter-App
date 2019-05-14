2d5d380c613f2ea9297dfc5444d81e94b7507f7b
michael-ziedalski <43689829+michael-ziedalski@users.noreply.github.com>
2019-05-14 13:51:22 -0400

    tiny change

--- table_and_graph_functions.py
+++ table_and_graph_functions.py
@@ -1,3 +1,7 @@
+from file_read_backwards import FileReadBackwards
+import plotly.figure_factory as ff
+
+
 ## Function to concurrently read 1) csv from bottom and 2) live table from top, 
 ## and perform what updates need to be done.
 def read_csv_backwards(file_name, rows, lines):
