ca2e0285eec7b8fac1b97a19ea4fee209d1feeea
michael-ziedalski <43689829+michael-ziedalski@users.noreply.github.com>
2019-05-14 13:42:44 -0400

    Update main_code.py
    Corresponding changes to main_code once two functions had been moved into table_and_graph_functions

--- main_code.py
+++ main_code.py
@@ -29,6 +29,7 @@ import flask
 ## Custom functions
 from twitter_functions import id_extractor, old_tweets
 from sentiment_analysis_functions import tweet_cleaning, tweet_sentiment
+from table_and_graph_functions import read_csv_backwards, generate_trace_per_author
 
 
 ###### Setup ######
@@ -284,31 +285,10 @@ def tweet_table(Table_Update_Component, Submit_Button, max_intervals, ScreenName
                 if lines <2:
                                         
                     return rows, dash.no_update, 2
-            
+                
 
                 ## Function to concurrently read 1) csv from bottom and 2) live table from top, 
                 ## and perform what updates need to be done.
-                def read_csv_backwards(file_name, rows, lines):
-                    newest_tweets = []
-                    
-                    with FileReadBackwards(file_name) as fb:
-                        j = 0
-                        for i, line in enumerate(fb):
-                            line_formatted = line.split(sep=',')
-                            line_formatted = {'Date': line_formatted[0], 'Author': line_formatted[1], 'Len': line_formatted[2], 
-                                              'Favs': line_formatted[3], 'Retw': line_formatted[4], 'Text': line_formatted[5]}  
-
-                            if (line_formatted == rows[i]) or (line_formatted['Date'] == 'Date'):
-                                print("MATCH!!!!")
-                                return rows, newest_tweets
-
-                            elif line_formatted != rows[i]:
-                                newest_tweets.append(line_formatted)
-                                rows.insert(i+j, line_formatted)
-                                j = j + 1
-
-                                    
-                ## New function needed to take advantage of return being able to break out of nested loops
                 rows, newest_tweets = read_csv_backwards(csv_name, rows, lines)
                                 
                 print('\n\nNEWEST_TWEETS: \n{}\n'.format(newest_tweets))    
@@ -371,62 +351,15 @@ def enable_disable_interval(Table_Exists_Or_Not_Check, ScreenName_Input, Stream_
     ## Semi-circular hack below
     State(component_id='Graph_trace_indices', component_property='data')],
 )
-def sentiment_selected_tweets(Newest_Tweets, tweet_table, kde_checklist, Graph_trace_indices):
-    
-    def generate_trace_per_author(tweets_dict, Graph_trace_indices, create_or_append = 1):
-
-        x_input = list(tweets_dict.values())
-#         num_tweet_authors = len(x_input)
-
-        ## If traces are being created, either at start of app, or when picking new twitter accounts
-        if create_or_append == 1:
-            
-            ## Using automative plotly method to plot histograms + kde's at once
-            fig = ff.create_distplot(x_input, [i for i in tweets_dict.keys()], bin_size=.2, show_rug=False)
-            
-            traces = []
-            for i in fig['data']:
-                traces.append(i)
-            
-            ## To create a dict carrying the index of each trace, 
-            ## so future tweets know which trace to go into
-            trace_indices = {}
-            for i, j in enumerate(tweets_dict.keys()):
-                trace_indices[j] = i
-
-            ## Slight visual editing
-            for i in traces:
-                if isinstance(i, plotly.graph_objs._histogram.Histogram):
-                    i['xbins'] = {'end': 1, 'size': 0.2, 'start': -1.0}
-
-            figure = {'data': traces}
-
-            return figure, trace_indices
-        
-        ## Or appending to existing traces
-        elif create_or_append == 2:
-            fig = {'data': [{'x': i} for i in x_input]}
-          
-            traces = []
-            for i in fig['data']:
-                traces.append(i)
-            
-            traces = [traces]
-
-            trace_indices = []
-            for i in tweets_dict.keys():
-                trace_indices.extend([Graph_trace_indices[i]])
-                
-            traces.append(trace_indices)
-
-            return traces
+def sentiment_grapher(Newest_Tweets, tweet_table, kde_checklist, Graph_trace_indices):
     
     ## New stream has just started, and new graph needs to be made
     if Newest_Tweets == 1:
         
         tweets_sentiment = tweet_sentiment(tweet_table)        
 
-        # Tweets' sentiments grouped by Author and then fed in to make traces
+        ## Tweets' sentiments grouped by Author and then fed in to make traces;
+        ## creating traces that Dash/plotly can make graphs out of
         traces, trace_indices = generate_trace_per_author(tweets_sentiment, [], 1)
 
         return dash.no_update, traces, trace_indices
