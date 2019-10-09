dcd72750658611920cecb5b9444481ddf0d47b79
michael-ziedalski <43689829+michael-ziedalski@users.noreply.github.com>
2019-10-09 11:11:11 -0400

    Update sentiment_analysis_functions.py
    Will add later

--- sentiment_analysis_functions.py
+++ sentiment_analysis_functions.py
@@ -4,14 +4,27 @@ from textblob import TextBlob
 
 
 ## Organizing tweet by id
-def tweet_id_categorizing(tweets):
+def tweet_id_categorizing(tweets, only_keywords_search):
+
+    # if not tweets:
+    #     print('\nNo tweets to analyze.')
+    #     return
+
     categorized_tweets = {}
-    
-    for tweet in tweets:
-        if tweet['Author'] in categorized_tweets:
-            categorized_tweets[tweet['Author']].append(tweet['Text'])
-        else:
-            categorized_tweets[tweet['Author']] = [tweet['Text']]
+
+    ## If tweets should be differentiated by user
+    if only_keywords_search == 0:
+
+        for tweet in tweets:
+            if tweet['Author'] in categorized_tweets:
+                categorized_tweets[tweet['Author']].append(tweet['Text'])
+            else:
+                categorized_tweets[tweet['Author']] = [tweet['Text']]
+
+    ## If tweets should be not differentiated by user, such as searching only by keyword
+    elif only_keywords_search == 1:
+
+        categorized_tweets['all tweets'] = [i['Text'] for i in tweets]
     
     # Turning tweet collection for each author into np.array for efficient cleaning in next f.        
     print('\n\ntweet_id_categorizing:\n{}\n'.format(categorized_tweets))
@@ -20,7 +33,11 @@ def tweet_id_categorizing(tweets):
 
 
 ## Next is automatic cleaning of each tweet for textblob's use
-def tweet_cleaning(categorized_tweets): 
+def tweet_cleaning(categorized_tweets):
+
+    # if not categorized_tweets:
+    #     print('\nNo tweets to analyze.')
+    #     return
     
     for authors_tweets in categorized_tweets.keys():
         for tweet in categorized_tweets[authors_tweets]:
@@ -35,9 +52,9 @@ def tweet_cleaning(categorized_tweets):
 
 
 ## Sentiment analysis on previous functions' output using textblob
-def tweet_sentiment(tweets):
+def tweet_sentiment(tweets, only_keywords_search = 0):
     
-    categorized_tweets = tweet_id_categorizing(tweets)
+    categorized_tweets = tweet_id_categorizing(tweets, only_keywords_search)
     categorized_cleaned_tweets = tweet_cleaning(categorized_tweets)
 
     if isinstance(categorized_cleaned_tweets, dict):
