25720bf49bb70d810c1b89f848a0002f397f668d
michael-ziedalski <43689829+michael-ziedalski@users.noreply.github.com>
2019-10-09 11:11:31 -0400

    Update table_and_graph_functions.py
    Will add later

--- table_and_graph_functions.py
+++ table_and_graph_functions.py
@@ -12,6 +12,9 @@ def read_csv_backwards(file_name, rows, lines):
         j = 0
         for i, line in enumerate(fb):
             line_formatted = line.split(sep=',')
+
+            print('\n\nCHECKING:\n{}\n\n'.format(line_formatted))
+            
             line_formatted = {'Date': line_formatted[0], 'Author': line_formatted[1], 'Len': line_formatted[2], 
                               'Favs': line_formatted[3], 'Retw': line_formatted[4], 'Text': line_formatted[5]}  
 
@@ -27,12 +30,17 @@ def read_csv_backwards(file_name, rows, lines):
                 
 
 ## Creating traces that Dash/plotly can make graphs out of
-def generate_trace_per_author(tweets_dict, Graph_trace_indices, create_or_append = 1):
+def generate_trace_per_author(tweets_dict, Graph_trace_indices, create_or_append = 1, Only_Keywords_Search = 0):
 
     x_input = list(tweets_dict.values())
 
     ## If traces are being created, either at start of app, or when picking new twitter accounts
     if create_or_append == 1:
+
+
+        # print("\n\nCREATING trace: x_input\n{}\n\n".format(x_input))
+
+
 
         ## Using automative plotly method to plot histograms + kde's at once
         fig = ff.create_distplot(x_input, [i for i in tweets_dict.keys()], bin_size=.2, show_rug=False)
@@ -47,11 +55,23 @@ def generate_trace_per_author(tweets_dict, Graph_trace_indices, create_or_append
         for i, j in enumerate(tweets_dict.keys()):
             trace_indices[j] = i
 
+
+
+        print("\n\nCREATING trace_indices:\n{}\n\n".format(trace_indices))
+
+
+
         ## Slight visual editing
         for i in traces:
             if isinstance(i, plotly.graph_objs.Histogram):
                 i['xbins'] = {'end': 1, 'size': 0.2, 'start': -1.0}
                 i['histnorm'] = 'probability'
+
+
+
+        # print("\n\nCREATING trace: traces\n{}\n\n".format(traces))
+
+
 
         figure = {'data': traces}
 
@@ -59,18 +79,55 @@ def generate_trace_per_author(tweets_dict, Graph_trace_indices, create_or_append
 
     ## Or appending to existing traces
     elif create_or_append == 2:
-        fig = {'data': [{'x': i} for i in x_input]}
 
-        traces = []
-        for i in fig['data']:
-            traces.append(i)
+        ## Check if tweets should be grouped by author, such as when not searching purely by keyword
+        if Only_Keywords_Search == 0:
 
-        traces = [traces]
+            # print("\n\nAPPENDING trace: x_input\n{}\n\n".format(x_input))
 
-        trace_indices = []
-        for i in tweets_dict.keys():
-            trace_indices.extend([Graph_trace_indices[i]])
 
-        traces.append(trace_indices)
+            fig = {'data': [{'x': i} for i in x_input]}
 
-        return traces
+            traces = []
+            for i in fig['data']:
+                traces.append(i)
+
+            traces = [traces]
+
+
+            print("\n\nAPPENDING trace: trace itself\n{}\n\n".format(traces))
+
+
+            # print("\n\nAPPENDING trace: traces\n{}\n\n".format(traces))
+
+
+            trace_indices = []
+            for i in tweets_dict.keys():
+                trace_indices.extend([Graph_trace_indices[i]])
+
+            traces.append(trace_indices)
+
+
+            print("\n\nAPPENDING trace: trace_indices\n{}\n\n".format(trace_indices))
+
+
+            return traces
+
+        ## Searching by pure keywords
+        elif Only_Keywords_Search == 1:
+
+            traces = [[{'x': x_input[0]}]] ## x_input has some unnecessary nested list, so getting out with [0]
+            trace_indices = [0]
+
+
+
+            print("\n\nAPPENDING trace: trace itself\n{}\n\n".format(traces))
+
+            print("\n\nAPPENDING trace: trace_indices\n{}\n\n".format(trace_indices))
+
+
+
+            traces = traces.append(trace_indices)
+
+            return traces
+
